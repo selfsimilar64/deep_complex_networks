dbacc74c47ba26f29fefe57cd2ef177d6cdf143d
Matt <mattjkelley710@gmail.com>
2018-04-26 16:25:11 -0400

    Direct Real/Imag Split

--- scripts/training.py
+++ scripts/training.py
@@ -505,12 +505,14 @@ def tanh(Z):
     return K.tanh(Z)
 
 def tanh_z(Z):
-    X = GetReal()(Z)
-    Y = GetImag()(Z)
+    input_dim = K.shape(Z)[1] // 2
+    X = Z[:, :input_dim]
+    Y = Z[:, input_dim:]
     A = K.T.cosh(2 * X) + K.cos(2 * Y)
     U = K.T.sinh(2 * X) / A
     V = K.sin(2 * Y) / A
     W = K.concatenate([U, V], axis=1)
+
     return W
 
 #
