186fb10a62ad800cac8d1c9c0ca767f533ef2fb3
Matt <mattjkelley710@gmail.com>
2018-05-01 20:30:22 -0400

    l

--- scripts/training.py
+++ scripts/training.py
@@ -791,7 +791,7 @@ def train(d):
             im_num = 55
             channel_x = 5
             channel_y = (pre_act.shape[1] // 2) + channel_x
-            im = np.squeeze(X_val[im_num, :, :, :])
+            im = np.squeeze(X_val[im_num, :, :, :]).reshape(3*32, 32)
             im_real_prev = np.squeeze(pre_act[im_num, channel_x, :, :])
             im_imag_prev = np.squeeze(pre_act[im_num, channel_y, :, :])
             im_mod_prev = np.sqrt(im_real_prev ** 2 + im_imag_prev ** 2)
